02f7ae2340747f9de2f5c0c1ed765093d0f97340
Gaurav <gauravtak996@gmail.com>
2022-07-17 16:55:29 +0530

    card section added

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-alert("hello waht up!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using jquery
+// const pleaseDonate = $(".nav-2-button");
+
+// pleaseDonate.click(function () {
+//     prompt("Allah Ke Naam Pe Daide");
+//   });
+
+
+
+
+
+//  const pleaseDonate = document.querySelector("nav-2-button");
+
+// pleaseDonate.addEventListener("click",function () {
+//     alert("How much you want to donate?");
+// });
+
+
