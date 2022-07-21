8d5d052b24c69be0ff5c39b437c4b9d5b3275ef7
Gaurav <gauravtak996@gmail.com>
2022-07-21 19:21:05 +0530

    index.js modified 4.0

--- index.js
+++ index.js
@@ -1,96 +1,164 @@
-// $(function () {
-//     for (let i = 1; i <= 9; i++) {
-//         var $cards = $(".cards")
+$(function () {
+    for (let i = 1; i <= 9; i++) {
+        var $cards = $(".cards")
         
-//         $.ajax({
-//             type: 'GET',
-//             url: 'https://raw.githubusercontent.com/gaurav-2-0-0-2/Blog-template/main/cards.json',
-//             success: function (data) {
-//                 $.each($cards, function (i, card) {
-//                     $cards.append(`<div class="card">
+        $.ajax({
+            type: 'GET',
+            url: 'https://raw.githubusercontent.com/gaurav-2-0-0-2/Blog-template/main/cards.json',
+            success: function (data) {
+                $.each($cards, function (i, card) {
+                    $cards.append(`<div class="card">
     
-//                     <img class="card-img" src="images/one.jpg" alt="Avatar">
-//                     <div class="date-tag">
-//                         <h6>24 MAY</h6>
-//                     </div>
-//                     <div class="container-1_1">
-                    
-//                         <a href="#"><i class="fa-solid fa-user user-icon"></i></a>
-//                         <h6 class="user-icon-h6">By: Admin</h6>
-//                         <a href="#"><i class="fa-solid fa-heart heart-icon"></i></a>
-//                         <h6 class="heart-icon-h6">15</h6>
-        
-//                         <a href="#"><i class="fa-solid fa-comment comment-icon"></i></a>
-//                         <h6 class="comment-icon-h6">25</h6>
-//                         <h6 class="heart-icon-h6">15</h6>
-        
-//                         <h4>Lorem ipsum dolor sit amet</h4>
-//                         <p>Lorem ipsum dolor sit amet, consectetur adipicing </p>
-//                         <p>elit. Nullam feugiat dignissim metus, id vestibulum</p>
-//                         <p>tellus Cras ante commodo.</p>
-        
-//                         <a href="#" class="read-more">Read More...</a>
-//                     </div>
-//                 </div>`
-                
-                
-            
-//                 )
-        
-//                })
-//             }
-//         })
-        
-//     }
-    
-
-
-
-
-
-
-
-
-
-
-
-for(let i=1; i<=9; i++){
-    let cards = document.querySelector(".cards");
-
-    let card = document.createElement('div')
-    card.classList.add('card', `card${i}`)
-    card.innerHTML = 
-
-    `
-    
-    
-                    <img class="card-img" src="" alt="Avatar">
+                    <img class="card-img" src="images/one.jpg" alt="Avatar">
                     <div class="date-tag">
-                        <h6></h6>
+                        <h6>24 MAY</h6>
                     </div>
                     <div class="container-1_1">
-                       
+                    
                         <a href="#"><i class="fa-solid fa-user user-icon"></i></a>
-                        <h6 class="user-icon-h6"></h6>
+                        <h6 class="user-icon-h6">By: Admin</h6>
                         <a href="#"><i class="fa-solid fa-heart heart-icon"></i></a>
-                        
+                        <h6 class="heart-icon-h6">15</h6>
         
                         <a href="#"><i class="fa-solid fa-comment comment-icon"></i></a>
-                        <h6 class="comment-icon-h6"></h6>
-                        <h6 class="heart-icon-h6"></h6>
+                        <h6 class="comment-icon-h6">25</h6>
+                        <h6 class="heart-icon-h6">15</h6>
         
-                        <h4></h4>
-                        <p></p>
+                        <h4>Lorem ipsum dolor sit amet</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipicing </p>
+                        <p>elit. Nullam feugiat dignissim metus, id vestibulum</p>
+                        <p>tellus Cras ante commodo.</p>
         
                         <a href="#" class="read-more">Read More...</a>
                     </div>
+                </div>`
+                
+                    
+            
+                )
+        
+               })
+            }
+        })
+        
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i=1; i<=9; i++){
+//     let cards = document.querySelector(".cards");
+
+//     let card = document.createElement('div')
+//     card.classList.add('card', `card${i}`)
+//     card.innerHTML = 
+
+//     `
+    
+    
+//                     <img class="card-img" src="" alt="Avatar">
+//                     <div class="date-tag">
+//                         <h6></h6>
+//                     </div>
+//                     <div class="container-1_1">
+                       
+//                         <a href="#"><i class="fa-solid fa-user user-icon"></i></a>
+//                         <h6 class="user-icon-h6"></h6>
+//                         <a href="#"><i class="fa-solid fa-heart heart-icon"></i></a>
+                        
+        
+//                         <a href="#"><i class="fa-solid fa-comment comment-icon"></i></a>
+//                         <h6 class="comment-icon-h6"></h6>
+//                         <h6 class="heart-icon-h6"></h6>
+        
+//                         <h4></h4>
+//                         <p></p>
+        
+//                         <a href="#" class="read-more">Read More...</a>
+//                     </div>
                
-    `
+//     `
 
-    cards.append(card);
-}
-
-
+//     cards.append(card);
+// }
 
 
 
@@ -99,44 +167,21 @@ for(let i=1; i<=9; i++){
 
 
 
-
-
-
-
-
-
-
-
-
-// fetch('https://raw.githubusercontent.com/gaurav-2-0-0-2/Blog-template/main/cards.json')
-//     .then(result => result.json())
-//     .then((output) => {
+// fetch("https://raw.githubusercontent.com/gaurav-2-0-0-2/Blog-template/main/cards.json")
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
 //         for (let i = 1; i <= 9; i++) {
-//             document.querySelector(`.card${i} .container-1_1 p`).textContent = output[i-1].desc
-//             document.querySelector(`.card${i} .container-1_1 h4`).textContent = output[i-1].title
-//             document.querySelector(`.card${i} .user-icon-h6`).textContent = output[i-1].admin
-//             document.querySelector(`.card${i} .heart-icon-h6`).textContent = output[i-1].like_count
-//             document.querySelector(`.card${i} .comment-icon-h6`).textContent = output[i-1].comment_count
-//             document.querySelector(`.card${i} .images/one.jpg img`).src = output[i-1].img
-//             document.querySelector(`.container${i} .date-tag`).textContent = output[i-1].datetime
+//             document.querySelector(`.card${i} .container-1_1 p`).textContent = data[i-1].desc
+//             document.querySelector(`.card${i} .container-1_1 h4`).textContent = data[i-1].title
+//             document.querySelector(`.card${i} .user-icon-h6`).textContent = data[i-1].admin
+//             document.querySelector(`.card${i} .heart-icon-h6`).textContent = data[i-1].like_count
+//             document.querySelector(`.card${i} .comment-icon-h6`).textContent = data[i-1].comment_count
+//             document.querySelector(`.card${i} .card-img`).src = data[i-1].img
+//             document.querySelector(`.card${i} .date-tag`).textContent = data[i-1].datetime
 //         }
 //     }).catch(err => console.error(err));
-
-fetch("https://raw.githubusercontent.com/gaurav-2-0-0-2/Blog-template/main/cards.json")
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        for (let i = 1; i <= 9; i++) {
-            document.querySelector(`.card${i} .container-1_1 p`).textContent = data[i-1].desc
-            document.querySelector(`.card${i} .container-1_1 h4`).textContent = data[i-1].title
-            document.querySelector(`.card${i} .user-icon-h6`).textContent = data[i-1].admin
-            document.querySelector(`.card${i} .heart-icon-h6`).textContent = data[i-1].like_count
-            document.querySelector(`.card${i} .comment-icon-h6`).textContent = data[i-1].comment_count
-            document.querySelector(`.card${i} .card-img`).src = data[i-1].img
-            document.querySelector(`.card${i} .date-tag`).textContent = data[i-1].datetime
-        }
-    }).catch(err => console.error(err));
 
 
 
